022bcc518b0f3b190f164d3eb21df921f23e02cf
weerayosong <weerayosong@outlook.com>
2026-03-10 18:09:34 +0700

    modified 06_file

--- 06_bring-it-to-javascript.js
+++ 06_bring-it-to-javascript.js
@@ -1,43 +1,55 @@
-let product1 = {
+/* Product Array */
+const product = [
+    {
+        product_id: 1,
+        product_name: "DY Cloth Cap",
+        inStock: true,
+        color: ["Black", "White", "Navy"],
+        price: 159
+    },
+    {
+        product_id: 2,
+        product_name: "Denim Cap",
+        inStock: false,
+        color: ["Black", "Light Jeans", "Dark Jeans"],
+        price: 199
+    },
+    {
+        product_id: 3,
+        product_name: "Mesh Cap",
+        inStock: true,
+        color: ["Black", "White", "Two-Tone"],
+        price: 219
+    }
+];
+
+/* Order Object */
+const order = {
+  order_id: 1,
+  user_id: 1,
+  total_amount: 378,
+  status: "pending",
+  shipping_address: "1/1 moo.1, Bang Phli, Samut Prakarn 10540"
+};
+
+/* Array of Order objects for a cart */
+const cart = [
+  {
     product_id: 1,
     product_name: "DY Cloth Cap",
-    inStock: true,
-    color: ["Black", "White", "Navy"],
-    price: 159
-};
-let product2 = {
-    product_id: 2,
-    product_name: "Denim Cap",
-    inStock: false,
-    color: ["Black", "Light Jeans", "Dark Jeans"],
-    price: 199
-};
-let product3 = {
-    product_id: 3,
+    price: 159,
+    stock_quantity: 23,
+  },
+  { product_id: 3,
     product_name: "Mesh Cap",
-    inStock: true,
-    color: ["Black", "White", "Two-Tone"],
-    price: 219
-};
+    price: 219,
+    stock_quantity: 12 }
+];
 
-
-let order1 = `ProductID: ccz${product1.product_id}\nName: ${product1.product_name}\nisStock?: ${product1.inStock}\nColor: ${product1.color[0]}\nPrice: ${product1.price} THB`;
-let order2 = `ProductID: ccz${product2.product_id}\nName: ${product2.product_name}\nisStock?: ${product2.inStock}\nColor: ${product2.color[1]}\nPrice: ${product2.price} THB`;
-let order3 = `ProductID: ccz${product3.product_id}\nName: ${product3.product_name}\nisStock?: ${product3.inStock}\nColor: ${product3.color[2]}\nPrice: ${product3.price} THB`;
-
-let productList = [product1.product_name, product2.product_name, product3.product_name];
-
-let cart = {product1, product2, product3};
-
-console.log(`1. string for product name (Product1): ${product1.product_name}`);
-console.log(`2. number for price (Product2): ${product2.price} THB`);
-console.log(`3. boolean for inStock (Product3): ${product3.inStock} THB`);
-console.log(`4. array for list of products: ${productList}`);
-console.log(`5. object for one order: ${order1}`);
-console.log(`6. array of objects for shopping cart: `);
-console.log(cart);
-
-
-// console.log(order1);
-// console.log("###### Cart list ######");
-// console.log(cart);
+/* Output */
+console.log(`1. String for product name (Product1): ${product[0].product_name}`);
+console.log(`2. Number for price (Product2): ${product[1].price} THB`);
+console.log(`3. Boolean for inStock (Product3): ${product[2].inStock}`);
+console.log("4. Array for list of products: ", product);
+console.log("5. object for one order: ", order);
+console.log("6. array of objects for shopping cart: ", cart);
