e3be03db1a1e6755a0973d4ed73531d0bcc81600
weerayosong <weerayosong@outlook.com>
2026-03-11 17:00:35 +0700

    added exercise.sql file

--- postgresql/query.sql
+++ postgresql/query.sql
@@ -1,3 +1,4 @@
 -- SELECT * FROM Suppliers where supplier_id >= 2
 -- SELECT * FROM suppliers where supplier_id <= 2
-selECt * from ingREdients whERe inGreDiENT_id > 5
+-- selECt * from ingREdients whERe inGreDiENT_id > 5
+-- select * from recipeitems 
